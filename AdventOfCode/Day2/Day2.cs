1fcb59ccb8ee4112e037006db0fbbd698551a3a6
Andy Shaw <andrewoutramshaw@gmail.com>
2023-12-03 18:03:29 +0000

    Day2: create test for real part 2 (answer added after obtianing from failed test)

--- AdventOfCode/Day2/Day2.cs
+++ AdventOfCode/Day2/Day2.cs
@@ -214,13 +214,21 @@ public class Day2
         Assert.That(evaluator.Evaluate(ExampleData, exampleBag), Is.EqualTo(2286));
     }
 
-    public string ExampleData = @"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+    [Test]
+    public void Part2()
+    {
+        GameEvaluator evaluator = new GameEvaluator(
+            new GameInterpreter(), 
+            new GameBagMinimiser(), 
+            new MinimumGameBagCombiner()
+        );
 
-    public string ExampleData2 = @"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Bag exampleBag = new Bag();
+
+        Assert.That(evaluator.Evaluate(Data, exampleBag), Is.EqualTo(66909));
+    }
+
+    public string ExampleData = @"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
