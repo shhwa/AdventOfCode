777622b647770ef2b9eef7cdafeed6301dae9d18
Andy Shaw <andrewoutramshaw@gmail.com>
2023-12-03 17:31:58 +0000

    Day2: Added test for real question, answer inserted after failed test

--- AdventOfCode/Day2/Day2.cs
+++ AdventOfCode/Day2/Day2.cs
@@ -140,6 +140,25 @@ public class Day2
         Assert.That(evaluator.Evaluate(ExampleData, exampleBag), Is.EqualTo(8));
     }
 
+    [Test]
+    public void Part1()
+    {
+        GameEvaluator evaluator = new GameEvaluator(
+            new GameInterpreter(), 
+            new GameValidator(), 
+            new GameCombiner()
+        );
+
+        Bag bag = new Bag
+        {
+            NoRed = 12,
+            NoGreen = 13,
+            NoBlue = 14
+        };
+
+        Assert.That(evaluator.Evaluate(Data, bag), Is.EqualTo(2156));
+    }
+
     public string ExampleData = @"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
