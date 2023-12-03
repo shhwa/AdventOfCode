9fb416c059a22777ad01fd65764089e3da7ae948
Andy Shaw <andrewoutramshaw@gmail.com>
2023-12-03 18:01:36 +0000

    Day2: test with given example for part2

--- AdventOfCode/Day2/Day2.cs
+++ AdventOfCode/Day2/Day2.cs
@@ -195,7 +195,32 @@ public class Day2
         Assert.That(result, Is.EqualTo((2*2*2) + (3*3*3)));
     }
 
+    [Test]
+    public void Part2Example()
+    {
+        GameEvaluator evaluator = new GameEvaluator(
+            new GameInterpreter(), 
+            new GameBagMinimiser(), 
+            new MinimumGameBagCombiner()
+        );
+
+        Bag exampleBag = new Bag
+        {
+            NoRed = 12,
+            NoGreen = 13,
+            NoBlue = 14
+        };
+
+        Assert.That(evaluator.Evaluate(ExampleData, exampleBag), Is.EqualTo(2286));
+    }
+
     public string ExampleData = @"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    public string ExampleData2 = @"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
